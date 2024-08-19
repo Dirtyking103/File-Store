2b32a327eb0d1200692ce38273b1a808bb0e2fb0
Dirtyking103 <dirtyking103@gmail.com>
2024-08-19 13:24:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,13 +59,13 @@ SHORTLINK_URL = environ.get("SHORTLINK_URL", "instantlinks.com") # shortlink dom
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/cine_world9") # how to open link 
 
 # Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 WEBSITE_URL = environ.get("WEBSITE_URL", "https://dirtyking102.blogspot.com/2024/08/dirtyking.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
